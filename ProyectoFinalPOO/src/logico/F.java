37225a76cea1ff1a1a6d124f2a274e929182d08c
JosueGG04 <92964815+JosueGG04@users.noreply.github.com>
2022-07-04 15:16:29 -0400

    Creando clase F

---
+++ ProyectoFinalPOO/src/logico/F.java
@@ -0,0 +1,20 @@
+package logico;
+
+public class F {
+	private int  respeto;
+
+	public F(int respeto) {
+		super();
+		this.respeto = respeto;
+	}
+
+	public int getRespeto() {
+		return respeto;
+	}
+
+	public void setRespeto(int respeto) {
+		this.respeto = respeto;
+	}
+	
+	
+}
