735c36a0ea36e18756299c31368bff7da9d135dc
eduar <eduar@DESKTOP-1PE9JGC>
2022-07-04 22:50:38 -0400

    Creando clase C

--- ProyectoFinalPOO/src/logico/C.java
+++ ProyectoFinalPOO/src/logico/C.java
@@ -1,5 +1,20 @@
 package logico;
 
 public class C {
+	private int ki;
 
+	public C(int ki) {
+		super();
+		this.ki = ki;
+	}
+
+	public int getKi() {
+		return ki;
+	}
+
+	public void setKi(int ki) {
+		this.ki = ki;
+	}
+	
 }
+
