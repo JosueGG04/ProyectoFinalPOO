faf10da6d9710df015ebadf78a4042f1819c448e
demia <demia@192.168.100.3>
2022-07-27 00:22:19 -0400

    Registro empresa visual

--- ProyectoFinalPOO/src/visual/RegistroEmpresa.java
+++ ProyectoFinalPOO/src/visual/RegistroEmpresa.java
@@ -9,14 +9,22 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class RegistroEmpresa extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtIDEmpresa;
+	private JTextField txtNomDirector;
+	private JTextField txtTelDirector;
+	private JTextField txtCorreoDirector;
+	private JTextField txtRNC;
+	private JTextField txtCorreoEmpresa;
 	private JTextField txtNombre;
-	private JTextField txtDirector;
-	private JTextField txtTelefono;
+	private JTextField txtTelEmpresa;
 
 	/**
 	 * Launch the application.
@@ -35,7 +43,7 @@ public class RegistroEmpresa extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegistroEmpresa() {
-		setBounds(100, 100, 556, 326);
+		setBounds(100, 100, 669, 314);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -45,47 +53,99 @@ public class RegistroEmpresa extends JDialog {
 			contentPanel.add(panel, BorderLayout.CENTER);
 			panel.setLayout(null);
 			
-			JLabel lblIDEmpresa = new JLabel("IDEmpresa:");
-			lblIDEmpresa.setBounds(10, 11, 79, 14);
-			panel.add(lblIDEmpresa);
+			JPanel panel_Empresa = new JPanel();
+			panel_Empresa.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informaci\u00F3n de la Empresa", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+			panel_Empresa.setBounds(10, 11, 623, 117);
+			panel.add(panel_Empresa);
+			panel_Empresa.setLayout(null);
 			{
-				txtIDEmpresa = new JTextField();
-				txtIDEmpresa.setBounds(75, 8, 170, 20);
-				panel.add(txtIDEmpresa);
-				txtIDEmpresa.setColumns(10);
+				JLabel lblRNC = new JLabel("RNC:");
+				lblRNC.setBounds(10, 29, 79, 14);
+				panel_Empresa.add(lblRNC);
+			}
+			{
+				JLabel lblCorreoEmpresa = new JLabel("Correo de la empresa:");
+				lblCorreoEmpresa.setBounds(10, 90, 129, 14);
+				panel_Empresa.add(lblCorreoEmpresa);
+			}
+			{
+				txtRNC = new JTextField();
+				txtRNC.setColumns(10);
+				txtRNC.setBounds(135, 26, 170, 20);
+				panel_Empresa.add(txtRNC);
+			}
+			{
+				txtCorreoEmpresa = new JTextField();
+				txtCorreoEmpresa.setColumns(10);
+				txtCorreoEmpresa.setBounds(135, 87, 476, 20);
+				panel_Empresa.add(txtCorreoEmpresa);
+			}
+			{
+				JLabel lblNombre = new JLabel("Nombre:");
+				lblNombre.setBounds(315, 29, 79, 14);
+				panel_Empresa.add(lblNombre);
+			}
+			{
+				JLabel lblTelEmpresa = new JLabel("Tel\u00E9fono de la empresa:");
+				lblTelEmpresa.setBounds(315, 62, 129, 14);
+				panel_Empresa.add(lblTelEmpresa);
 			}
 			{
 				txtNombre = new JTextField();
 				txtNombre.setColumns(10);
-				txtNombre.setBounds(350, 8, 170, 20);
-				panel.add(txtNombre);
+				txtNombre.setBounds(441, 26, 170, 20);
+				panel_Empresa.add(txtNombre);
 			}
 			{
-				JLabel lblNombre = new JLabel("Nombre:");
-				lblNombre.setBounds(285, 11, 79, 14);
-				panel.add(lblNombre);
+				txtTelEmpresa = new JTextField();
+				txtTelEmpresa.setColumns(10);
+				txtTelEmpresa.setBounds(441, 59, 170, 20);
+				panel_Empresa.add(txtTelEmpresa);
 			}
 			{
-				JLabel lblDirector = new JLabel("Director:");
-				lblDirector.setBounds(10, 44, 79, 14);
-				panel.add(lblDirector);
+				JLabel lblArea = new JLabel("\u00C1rea:");
+				lblArea.setBounds(10, 62, 46, 14);
+				panel_Empresa.add(lblArea);
 			}
+			
+			JComboBox cmbArea = new JComboBox();
+			cmbArea.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Industrial", "Comercial", "Servicios"}));
+			cmbArea.setEditable(true);
+			cmbArea.setBounds(135, 57, 170, 20);
+			panel_Empresa.add(cmbArea);
 			{
-				txtDirector = new JTextField();
-				txtDirector.setColumns(10);
-				txtDirector.setBounds(75, 41, 170, 20);
-				panel.add(txtDirector);
-			}
-			{
-				JLabel lblTelefono = new JLabel("Tel\u00E9fono:");
-				lblTelefono.setBounds(285, 42, 79, 14);
-				panel.add(lblTelefono);
-			}
-			{
-				txtTelefono = new JTextField();
-				txtTelefono.setColumns(10);
-				txtTelefono.setBounds(350, 39, 170, 20);
-				panel.add(txtTelefono);
+				JPanel panel_1 = new JPanel();
+				panel_1.setBorder(new TitledBorder(null, "Informaci\u00F3n del Director", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				panel_1.setBounds(10, 139, 623, 87);
+				panel.add(panel_1);
+				panel_1.setLayout(null);
+				
+				JLabel lbl = new JLabel("Director:");
+				lbl.setBounds(10, 31, 79, 14);
+				panel_1.add(lbl);
+				
+				JLabel lblCorreoDelDirector = new JLabel("Correo del Director:");
+				lblCorreoDelDirector.setBounds(10, 59, 115, 14);
+				panel_1.add(lblCorreoDelDirector);
+				
+				txtNomDirector = new JTextField();
+				txtNomDirector.setBounds(135, 28, 170, 20);
+				panel_1.add(txtNomDirector);
+				txtNomDirector.setColumns(10);
+				
+				txtCorreoDirector = new JTextField();
+				txtCorreoDirector.setBounds(135, 56, 476, 20);
+				panel_1.add(txtCorreoDirector);
+				txtCorreoDirector.setColumns(10);
+				
+				JLabel lblTelfonoDelDirector = new JLabel("Tel\u00E9fono del Director:");
+				lblTelfonoDelDirector.setBounds(315, 31, 129, 14);
+				panel_1.add(lblTelfonoDelDirector);
+				
+				txtTelDirector = new JTextField();
+				txtTelDirector.setBounds(441, 28, 170, 20);
+				panel_1.add(txtTelDirector);
+				txtTelDirector.setColumns(10);
 			}
 		}
 		{
